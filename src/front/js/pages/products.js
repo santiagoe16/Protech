75ea288aea1dfe7d91acb0f3ad53cf44794b2057
MDhz89 <mdjho89@gmail.com>
2024-10-28 23:02:13 +0000

    merge

--- src/front/js/pages/products.js
+++ src/front/js/pages/products.js
@@ -23,6 +23,7 @@ export const Products = () => {
             });
     };
 
+    //checar viewmore
     const getCategories = () => {
         fetch(`${process.env.BACKEND_URL}/api/categorias`)
             .then((response) => response.json())
