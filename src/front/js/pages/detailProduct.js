7b25d12e98590a1383fbba67111e1f7e3d22fe35
Jimmbo19 <jega190112@gmail.com>
2024-11-11 21:29:14 +0000

    dandole formato

--- src/front/js/pages/detailProduct.js
+++ src/front/js/pages/detailProduct.js
@@ -2,12 +2,14 @@ import React, { useEffect, useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import "../../styles/detailProduct.css";
+
 
 export const DetailProduct = () => {
     const { store, actions } = useContext(Context);
     const { id } = useParams();
     const [product, setProduct] = useState(null);
-    const [amounts, setAmounts] = useState({});
+
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -21,40 +23,59 @@ export const DetailProduct = () => {
 
     }, [id, store.products]);
 
-    
+
     return (
         <>
-            {product ? (
-                <div>
-                    <h2>{product.name}</h2>
-                    <p>{product.description}</p>
-                    <p>Price: ${product.price}</p>
-                    <img src={product.image} alt={product.name} />
-                    <input
-                        type="number"
-                        size={10}
-                        value={store.amounts[product.id] || ""}
-                        onChange={(e) => actions.handleAmountChangeflux(product.id, e.target.value)}
-                        placeholder="Amount"
-                        className="form-control"
-                        id="amount"
-                    />
-                    <button
-                        className="btn btn-primary mt-2"
-                        onClick={() => actions.addToCartFlux(product.id)}
-                    >Add to Cart</button>
+            <div className="container">
+                <div className="container ProductDetail">
+                    {product ? (
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          
+                            <div style={{ flex: 1, marginRight: '20px' }}>
+                                <img src={product.image} alt={product.name}  />
+                            </div>
+
+                            <div style={{ flex: 2 }}>
+                                <p>{product.category.name}</p>
+                                <h2>{product.name}</h2>
+                                <p>{product.description}</p>
+                                <p>Price: ${product.price}</p>
+                                <input
+                                    type="number"
+                                    size={10}
+                                    value={store.amounts[product.id] || ""}
+                                    onChange={(e) => actions.handleAmountChangeflux(product.id, e.target.value)}
+                                    placeholder="Amount"
+                                    className="form-control"
+                                    id="amount"
+                                />
+                                <button
+                                    className="btn btn-primary mt-2"
+                                    onClick={() => actions.addToCartFlux(product.id)}
+                                >
+                                    Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>Loading...</p>
+                    )}
                 </div>
-            ) : (
-                <p>Loading...</p>
-            )}
-            <div>
-                <h2>Productos</h2>
-                <ul>
-                    {store.products.map((product) => (
-                        <li key={product.id}>{product.name} - ${product.price}</li>
-                    ))}
-                </ul>
+                <div className="Detalles">
+                
+                               
+                               
+                </div>
+                <div>
+                    <h2>Productos</h2>
+                    <ul>
+                        {store.products.map((product) => (
+                            <li key={product.id}>{product.name} - ${product.price}</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
+
         </>
     );
 };
